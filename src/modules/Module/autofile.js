ae5fa48d06be6e5e438f2482ac5d190225650740
André Cruz <andremiguelcruz@msn.com>
2012-12-05 13:17:06 +0000

    Finish up module create task.

--- src/modules/Module/autofile.js
+++ src/modules/Module/autofile.js
@@ -1,17 +1,17 @@
 var path  = require('path');
 var utils = require('amd-utils');
+var glob  = require('glob');
+var async = require('async');
+var fs    = require('fs');
 
 var task = {
     id: 'spoon-module-create',
     name: 'SpoonJS module create',
     author: 'Indigo United',
     filter: function (opts) {
-        //opts.location = path.resolve(process.cwd(), opts.location);
         opts.name = utils.string.pascalCase(opts.name.replace(/_/g, '-'));
-        opts.nameSlug = utils.string.slugify(opts.dir);
+        opts.nameSlug = utils.string.slugify(opts.name);
         opts.dir = path.join(opts.location, opts.name);
-
-        // TODO: abort if the folder already exists
     },
     options: {
         name: {
@@ -20,10 +20,22 @@ var task = {
         },
         location: {
             description: 'The location which the module will be created',
-            'default': 'dsadsa'
+            'default': path.join(process.cwd(), 'src/Application')
         }
     },
     tasks: [
+        {
+            task: function (opt, next) {
+                fs.stat(opt.dir, function (err) {
+                    if (!err || err.code !== 'ENOENT') {
+                        next(new Error(opt.dir + ' already exists.'));
+                    }
+
+                    next();
+                });
+            },
+            description: 'Check if module already exists'
+        },
         {
             task: 'mkdir',
             description: 'Create the module directory',
@@ -43,8 +55,34 @@ var task = {
             task: 'scaffolding-file-rename',
             description: 'Rename files based on the name of the module',
             options: {
-                dir: '{{dir}}'
+                dir: '{{dir}}',
+                what: 'name',
+                'with': '{{name}}'
             }
+        },
+        {
+            task: 'scaffolding-file-rename',
+            description: 'Rename files based on the name of the module (hyphenated)',
+            options: {
+                dir: '{{dir}}',
+                what: 'hyphenated_name',
+                'with': '{{nameSlug}}'
+            }
+        },
+        {
+            task: function (opt, next) {
+                glob(opt.dir + '/**/.gitkeep', function (err, files) {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    async.forEach(files, function (file, next) {
+                        fs.unlink(file, next);
+                    }, next);
+                });
+            },
+            description: 'Cleanup dummy files'
+
         }
     ]
 };
