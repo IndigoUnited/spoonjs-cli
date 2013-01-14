4bb54d332b8a0b784eccc8b410996a72a21bced0
André Cruz <andremiguelcruz@msn.com>
2013-01-14 00:53:48 +0000

    Integrate update-notifier.

--- bin/spoon.js
+++ bin/spoon.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 
 var Engine = require('../src/Engine'),
+    updateNotifier = require('update-notifier'),
+    notifier,
     spooncli;
 
-spooncli = new Engine(process.argv);
+// Checks for available update
+var notifier = updateNotifier({
+    packagePath: __dirname + '/../package'
+});
+if (notifier.update) {
+    notifier.notify();
+}
 
+// Initialize the engine
+spooncli = new Engine(process.argv);
 spooncli.parse();
