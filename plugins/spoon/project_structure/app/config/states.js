4df3d26df94f635af77e04254185e271c5e6a68c
André Cruz <andremiguelcruz@msn.com>
2013-06-26 13:35:59 +0000

    Remove stats from project structure.

--- plugins/spoon/project_structure/app/config/states.js
+++ plugins/spoon/project_structure/app/config/states.js
@@ -6,18 +6,6 @@ define(function () {
     // This is actually not necessary if you don't have routes associated to states
     // But it is a good practice to do so, because it gives an overview of all the application states
     return {
-        home: '/',
-        articles: {
-            index: '/',
-            // Just to demonstrate some advanced stuff
-            show: {
-                $pattern: '/{id}',
-                $constraints: {
-                    id: /\d+/
-                }
-            }
-        },
-        help: '/help',
-        about: '/about'
+        home: '/'
     };
 });
